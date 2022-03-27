02d4d569e6d652c8afce04b625a53d322ae96949
Hernan <hernangut96@gmail.com>
2022-03-27 07:02:15 -0300

    Mad Libs Game

---
+++ mad_libs_game.py
@@ -0,0 +1,23 @@
+noun=input('Enter a noun')
+plural_noun=input('Enter a plural noun')
+verb=input('Enter a verb')
+verb2=input('Enter another verb')
+part_of_body=input('Enter a part of body')
+adjective=input('Enter an adjective')
+plural_noun2=input('Enter another plural noun')
+adjective2=input('Enter another adjective')
+
+
+story=("               THE MAGIC COMPUTERS \n"
+
+"Today, every student has a computer small enough to fit into"
+" his "+noun+".\nHe can solve any math problem by simply pushing the "
+"computer's little " +plural_noun+ ".\nComputers can add, multiply, divide and"
+" "+verb+".\nThey can also "+verb2+" better than a human.\nSome computers are"
+" "+part_of_body+".\nOthers have a/an "+adjective+" screen that shows all kinds "+plural_noun2+""
+"and "+adjective+" figures.")
+
+
+
+
+print(story)
